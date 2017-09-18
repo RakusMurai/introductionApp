1a8ade86dd549259471104a8e17d32f9d2cb8487
dqlovers <dqlovers@DESKTOP-EJLUB3M>
2017-09-18 23:48:34 +0900

    App新規立ち上げ

---
+++ src/main/java/jp/co/rakus/introduction/IntroductionApp.java
@@ -0,0 +1,12 @@
+package jp.co.rakus.introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionApp.class, args);
+	}
+}
---
+++ src/test/java/jp/co/rakus/introduction/IntroductionAppTests.java
@@ -0,0 +1,16 @@
+package jp.co.rakus.introduction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntroductionAppTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
